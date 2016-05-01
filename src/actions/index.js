58c7c6668a561ed58015f57773cbedcbf8310479
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-05-01 14:39:48 -0400

    handle sign in success and save token localStorge

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,20 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 const ROOT_URL = 'http://localhost:3090';
 
 export function siginUser( { email, password }) {
   return function(dispatch) {
     //submit email and password to server
-    
-    // if request is good.
-    // - Update state to indicate user is authenticated
-    // - Save JWT token
-    // - redirect to the route '/feature'
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
 
-    // if bad reqest
-    // - show an error to the user
+      });
   }
 }
 
